f7130a3854ad0fc7c5d6a9bb8535eb46f4d46de0
Jasonzj <925645402@qq.com>
2018-05-03 01:58:26 +0800

    [comment]: add

--- src/js/board.ts
+++ src/js/board.ts
@@ -4,39 +4,64 @@ interface Cell {
   number: number
 }
 
-export const moveConfig: Object = {
+export const moveConfig: Object = {  // keycode对应方向的移动函数数组对象
   37: ['shiftLeft', 'combinLeft'],
   38: ['rotateLeft', 'shiftLeft', 'combinLeft', 'rotateRight'],
   39: ['shiftRight', 'combinRight'],
   40: ['rotateLeft', 'shiftRight', 'combinRight', 'rotateRight']
 }
 
+/**
+ * Board
+ * 游戏对象
+ * @class Board
+ */
 export default class Board {  
-  eleMap: HTMLTableCellElement[][]
-  map: number[][] = [
+  eleMap: HTMLTableCellElement[][]  // 二维数组dom地图(矩阵) 
+  map: number[][] = [               // 二维数组数字地图(矩阵)
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0]
   ]
-  
+
+  /**
+   * Creates an instance of Board.
+   * @param {HTMLTableElement} ele 游戏区域board Dom元素
+   * @param {Function} setScore 设置得分函数
+   * @memberof Board
+   */
   constructor(
     public ele: HTMLTableElement,
     public setScore: Function
   ) {}
 
+  /**
+   * init
+   * 初始化游戏
+   * @param {boolean} isInitCellMap 是否初始化Dom地图
+   * @param {number[][]} [map] 二维数组数字地图(矩阵)
+   * @memberof Board
+   */
   init(isInitCellMap: boolean, map?: number[][]) {
     isInitCellMap && this.initCellMap()
 
+    // 参数map存在则localStorage有数据，更新this.map并渲染
     if (map) {
       this.map = map
       this.render()
-    } else {
+    } else { // 生成二个随机方块(Cell)
       this.generateRandomCell()
       this.generateRandomCell()
     }
   }
 
+  /**
+   * clearCell
+   * 清除方块(Cell) 并重置坐标
+   * @param {boolean} [isSetMap=true] 是否重置坐标
+   * @memberof Board
+   */
   clearCell(isSetMap: boolean = true) {
     const { map, eleMap } = this
     for (let x = 0, l = map.length; x < l; x++) {
@@ -51,6 +76,11 @@ export default class Board {
     }
   }
 
+  /**
+   * initCellMap
+   * 初始化二维数组dom地图(矩阵) 与map对应
+   * @memberof Board
+   */
   initCellMap() {
     const { ele } = this
     const trs = Array.from(ele.querySelectorAll('tr'))
@@ -58,6 +88,15 @@ export default class Board {
     this.eleMap = trs.map((tr, i) => tds.splice(0, 4))
   }
 
+  /**
+   * runMove
+   * 运行移动函数 (根据keyCode来调用运行移动函数数组)
+   * @param {number} keyCode 键盘码
+   * @param {number[][]} prevMap 前一个map (根据prevMap判断map是否改变)
+   * @param {boolean} [isCheck=false] 是否是判断能否移动 (根据isCheck来区分运行和检查)
+   * @returns {boolean} 是否可以移动
+   * @memberof Board
+   */
   runMove(
     keyCode: number, 
     prevMap: number[][], 
@@ -69,30 +108,52 @@ export default class Board {
     return isMove
   }
 
+  /**
+   * checkGameOver
+   * 检查游戏是否结束
+   * @returns 游戏结果 (true为游戏结束)
+   * @memberof Board
+   */
   checkGameOver() {
     const prevMap: number[][] = JSON.parse(JSON.stringify(this.map))    
 
-    const isMoves: boolean[] = [
-      this.runMove(37, prevMap, true),
-      this.runMove(38, prevMap, true),
-      this.runMove(39, prevMap, true),
-      this.runMove(40, prevMap, true)
-    ]
+    // 将每个方向能否移动的结果存进数组
+    const isMoves: boolean[] = Object.keys(moveConfig)
+      .map(keyCode => this.runMove(parseInt(keyCode), prevMap, true))
 
     this.map = prevMap
     return isMoves.every(item => !item)
   }
 
+  /**
+   * setMap
+   * 设置map方块值
+   * @param {number} x x坐标
+   * @param {number} y y坐标
+   * @param {number} number 需要修改的值
+   * @memberof Board
+   */
   setMap(x: number, y: number, number: number) {
     this.map[x][y] = number
   }
 
+  /**
+   * generateRandomCell
+   * 生成随机方块(Cell)
+   * @memberof Board
+   */
   generateRandomCell() {
     const cell: Cell = this.getRandomCell()
     this.setMap(cell.x, cell.y, cell.number)
     this.render(cell.x, cell.y, cell.number)
   }
 
+  /**
+   * getRandomCell
+   * 获取随机方块(Cell)坐标和数字(2或4)
+   * @returns {Cell} 方块(Cell)坐标和数字对象
+   * @memberof Board
+   */
   getRandomCell(): Cell {
     const randomX: number = Math.floor(Math.random() * 4)
     const randomY: number = Math.floor(Math.random() * 4)
@@ -103,10 +164,23 @@ export default class Board {
       : this.getRandomCell()
   }
 
+  /**
+   * isBoardMove
+   * 判断调用移动函数后map是否改变
+   * @param {number[][]} prevMap 前一个map
+   * @param {number[][]} map 移动后的map
+   * @returns 是否改变 (true为可以移动)
+   * @memberof Board
+   */
   isBoardMove(prevMap: number[][], map: number[][]) {
     return JSON.stringify(prevMap) !== JSON.stringify(map)
   }
 
+  /**
+   * rotateRight
+   * map向右旋转
+   * @memberof Board
+   */
   rotateRight() {
     const { map } = this
     const newMap = []
@@ -123,6 +197,11 @@ export default class Board {
     this.map = newMap
   }
 
+  /**
+   * rotateLeft
+   * map向左旋转
+   * @memberof Board
+   */
   rotateLeft() {
     const { map } = this
     const newMap = []
@@ -139,6 +218,12 @@ export default class Board {
     this.map = newMap
   }
 
+  /**
+   * combinRight
+   * 向右合并数字
+   * @param {boolean} isCheck 是否正在检查gameover (检查中不能修改分数)
+   * @memberof Board
+   */
   combinRight(isCheck: boolean) {
     const { map } = this
     const len = map.length
@@ -156,7 +241,13 @@ export default class Board {
       }
     }
   }
-
+  
+  /**
+   * combinLeft
+   * 向左合并数字
+   * @param {boolean} isCheck 是否正在检查gameover (检查中不能修改分数)
+   * @memberof Board
+   */
   combinLeft(isCheck: boolean) {
     const { map } = this
     const len = map.length
@@ -175,6 +266,11 @@ export default class Board {
     }
   }
 
+  /**
+   * shiftRight
+   * 向右位移
+   * @memberof Board
+   */
   shiftRight() {
     const { map } = this
     const len = map.length
@@ -191,6 +287,11 @@ export default class Board {
     }
   }
 
+  /**
+   * shiftLeft
+   * 向左位移
+   * @memberof Board
+   */
   shiftLeft() {
     const { map } = this
     const len = map.length
@@ -207,18 +308,39 @@ export default class Board {
     }
   }
 
+  /**
+   * render
+   * 渲染函数 根据参数判断渲染方式 (参数存在为单个渲染)
+   * @param {number} [x] x坐标
+   * @param {number} [y] y坐标
+   * @param {number} [number] 需要渲染的值
+   * @memberof Board
+   */
   render(x?: number, y?: number, number?: number) {
     number 
       ? this.renderOneCell(x, y, number)
       : this.renderAllCell()
   }
 
+  /**
+   * renderOneCell
+   * 单个方块(Cell)渲染
+   * @param {number} x x坐标
+   * @param {number} y y坐标
+   * @param {number} number 需要渲染的值
+   * @memberof Board
+   */
   renderOneCell(x: number, y: number, number: number) {
     const cur = this.eleMap[x][y]
     cur.innerHTML = `${number}`
     cur.classList.add(`board__cell--${number}`)
   }
 
+  /**
+   * renderAllCell
+   * 全部方块(Cell)渲染
+   * @memberof Board
+   */
   renderAllCell() {
     const { map, eleMap } = this
 
--- src/js/control.ts
+++ src/js/control.ts
@@ -14,6 +14,11 @@ interface Touch {
   y: number
 }
 
+/**
+ * Control
+ * 游戏控制类
+ * @class Control
+ */
 export default class Control {
   board: Board
   score: Score
@@ -27,6 +32,15 @@ export default class Control {
   audioMove: HTMLAudioElement
   audioPopup: HTMLAudioElement
 
+  /**
+   * Creates an instance of Control.
+   * @param {HTMLTableElement} ele 游戏区域board Dom
+   * @param {HTMLElement} btnBox 按钮盒子Dom
+   * @param {HTMLElement} scoreEle 得分Dom
+   * @param {HTMLElement} scoreBestEle 最佳得分Dom
+   * @param {HTMLElement} maskEle gameover遮罩 Dom
+   * @memberof Control
+   */
   constructor(
     ele: HTMLTableElement,
     btnBox: HTMLElement,
@@ -34,45 +48,61 @@ export default class Control {
     scoreBestEle: HTMLElement,
     maskEle: HTMLElement
   ) {
-    this.ele = ele
-    this.btnBox = btnBox
-    this.speaker = true
-    this.moveState = false
-    this.board = new Board(ele, this.setScore)
-    this.audioMove = new Audio(moveAudio)
-    this.audioPopup = new Audio(popAudio)
-    this.score = { num: 0, ele: scoreEle }
-    this.bestScore = { num: 0, ele: scoreBestEle }
-    this.touch = { x: 0, y: 0 }
-    this.maskEle = maskEle
+    this.ele = ele // 游戏区域board Dom
+    this.btnBox = btnBox // 按钮盒子Dom
+    this.maskEle = maskEle // gameover遮罩 Dom
+    this.speaker = true // 扬声器状态 
+    this.moveState = false // 移动状态
+    this.board = new Board(ele, this.setScore) // board对象
+    this.audioMove = new Audio(moveAudio) // 移动声音
+    this.audioPopup = new Audio(popAudio) // 生成方块声音
+    this.touch = { x: 0, y: 0 } // 触摸事件坐标对象
+    this.score = { num: 0, ele: scoreEle } // 得分对象
+    this.bestScore = { num: 0, ele: scoreBestEle } // 最佳得分对象
     
+    // 初始化游戏
     this.init()
   }
 
+  /**
+   * init
+   * 初始化游戏
+   * @memberof Control
+   */
   init() {
     const { map, score, bestScore } = store.fetch('2048')
     
-    this.setEventBind()
+    this.setEventBind() // 事件绑定
     
     bestScore && this.setScore('bestScore', bestScore)
 
-    if (score) {
-      this.board.init(true, map)
-      this.setScore('score', score)
+    if (score) { // 根据score判断LocalStorage数据是否存在
+      this.board.init(true, map) // 初始化并渲染历史数据
+      this.setScore('score', score) // 设置历史得分
     } else {
-      this.board.init(true)
+      this.board.init(true) // 初始化board
     }
   }
 
+  /**
+   * reset
+   * 重置游戏
+   * @memberof Control
+   */
   reset() {
     const { board } = this
-    board.clearCell()
-    board.init(false)
-    this.setScore('score')
-    this.maskEle.className = 'mask'
-    store.save('2048', { bestScore: this.bestScore.num })
+    board.clearCell() // 清除方块(Cell) 并重置坐标
+    board.init(false) // 初始化board
+    this.setScore('score') // 初始化得分
+    this.maskEle.className = 'mask' // 隐藏遮罩
+    store.save('2048', { bestScore: this.bestScore.num }) // 保存最佳得分
   }
 
+  /**
+   * setScore
+   * 设置得分
+   * @memberof Control
+   */
   setScore = (scoreName: string, num?: number) => {
     num 
       ? this[scoreName].num += num
@@ -81,10 +111,21 @@ export default class Control {
     this.setScoreEle(scoreName)
   }
 
+  /**
+   * setScoreEle
+   * 设置得分dom
+   * @param {any} scoreName 
+   * @memberof Control
+   */
   setScoreEle(scoreName) {
     this[scoreName].ele.innerHTML = `${this[scoreName].num}`
   }
 
+  /**
+   * updateBestScore
+   * 更新最佳得分 (根据当前得分判断是否设置)
+   * @memberof Control
+   */
   updateBestScore() {
     const { score: { num }, bestScore: { num: bestNum } } = this
     const bestScore = num >= bestNum ? num : bestNum
@@ -92,20 +133,34 @@ export default class Control {
     this.setScoreEle('bestScore')
   }
 
+  /**
+   * setSpeaker
+   * 设置扬声器状态(执行一次取反)
+   * @param {HTMLElement} target 扬声器dom
+   * @memberof Control
+   */
   setSpeaker(target: HTMLElement) {
     this.speaker = !this.speaker
     const className = this.speaker ? 'btns__speaker--on' : 'btns__speaker--off'
     target.className = className
   }
 
+  /**
+   * move
+   * 移动函数 (根据keyCode判断移动方向)
+   * @param {number} keyCode 键盘码
+   * @memberof Control
+   */
   move(keyCode: number) {
     const { board, speaker, audioMove, audioPopup } = this
     const prevMap: number[][] = JSON.parse(JSON.stringify(board.map))
 
-    if (board.checkGameOver()) {
+    if (board.checkGameOver()) { // 检测游戏是否结束
       this.maskEle.className = 'mask--show'
+      return
     }
 
+    // 执行移动函数并返回能否移动结果
     const isMove: boolean = board.runMove(keyCode, prevMap)
 
     if (isMove) {
@@ -114,6 +169,7 @@ export default class Control {
       this.moveState = true    
       this.updateBestScore()
 
+      // 存储当前局势到LocalStorage
       const { bestScore } = store.fetch('2048')
       store.save('2048', {
         map: this.board.map,
@@ -129,19 +185,29 @@ export default class Control {
     }
   }
 
+  /**
+   * keyHandel
+   * 键盘事件处理
+   * @memberof Control
+   */
   keyHandel = (e) => {
     const { board } = this
     const { keyCode } = e
 
-    if (Object.keys(moveConfig).includes(`${keyCode}`))
+    if (Object.keys(moveConfig).includes(`${keyCode}`)) // 判断是否是上下左右
       this.move(keyCode)
   }
 
+  /**
+   * clickHandle
+   * 点击事件处理
+   * @memberof Control
+   */
   clickHandle = (e: MouseEvent) => {
     const target = e.target as HTMLElement
 
     if (target.nodeName === 'BUTTON') {
-      const eventFn: Function = {
+      const eventFn: Function = {  // 根据类名拿到处理函数
         'btns__speaker--on': this.setSpeaker,
         'btns__speaker--off': this.setSpeaker,
         'btns__reset': this.reset
@@ -151,8 +217,14 @@ export default class Control {
     }
   }
 
+  /**
+   * touchHandle
+   * 触摸事件处理
+   * @memberof Control
+   */
   touchHandle = (e: TouchEvent) => {
     e.preventDefault()
+    
     if (e.type === 'touchstart') {
       this.touch.x = e.touches[0].pageX
       this.touch.y = e.touches[0].pageY
@@ -160,19 +232,25 @@ export default class Control {
       const { x, y } = this.touch
       const endx = e.changedTouches[0].pageX
       const endy = e.changedTouches[0].pageY
-      const direction = getDirection(x, y, endx, endy)
-      const keyCode = { 1: 38, 2: 40, 3: 37, 4: 39 }[direction]
-      if (Object.keys(moveConfig).includes(`${keyCode}`))
+      const direction = getDirection(x, y, endx, endy) // 拿到滑动方向 1向上 2向下 3向左 4向右
+      const keyCode = { 1: 38, 2: 40, 3: 37, 4: 39 }[direction] // 根据滑动方向拿到对应的keyCode
+
+      if (Object.keys(moveConfig).includes(`${keyCode}`)) // 判断是否是上下左右
         this.move(keyCode)
     }
   }
 
+  /**
+   * setEventBind
+   * 事件绑定函数
+   * @memberof Control
+   */
   setEventBind() {
     addEvent(this.btnBox, 'click', this.clickHandle)
     addEvent(this.maskEle, 'click', this.clickHandle)
-    addEvent(document, 'keydown', this.keyHandel)
     addEvent(this.ele, 'touchstart', this.touchHandle)
     addEvent(this.ele, 'touchend', this.touchHandle)
+    addEvent(document, 'keydown', this.keyHandel)
   }
 
 }
